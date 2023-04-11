19268110e3924f10441776a94fc0c85aff455ed4
Sneha Yendluri <snye8655@colorado.edu>
2023-04-11 10:20:38 -0600

    added the collision detecton in ica12

--- js/ica12.js
+++ js/ica12.js
@@ -56,6 +56,21 @@ class Ball {
     this.x += this.velX;
     this.y += this.velY;
   }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+  
 }
 
 const testBall = new Ball(50, 100, 4, 4, "blue", 10);
@@ -90,9 +105,11 @@ function loop() {
   for (const ball of balls) {
     ball.draw();
     ball.update();
+    ball.collisionDetect();
   }
 
   requestAnimationFrame(loop);
 }
+
 
 loop();
